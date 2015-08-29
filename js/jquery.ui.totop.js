27009661879f6313efc317881b8e18b6f0739d3a
czekla <czekla@gmail.com>
2015-08-29 12:32:21 +0200

    totop

--- js/jquery.ui.totop.js
+++ js/jquery.ui.totop.js
@@ -15,15 +15,12 @@
         var containerIDhash = '#' + settings.containerID;
         var containerHoverIDHash = '#' + settings.containerHoverID;
 
-        $('body').append('<a href="#" id="' + settings.containerID + '" class="' + settings.containerClass + '" >' + settings.text + '</a>');
+        $('body').append('<span id="' + settings.containerID + '" class="' + settings.containerClass + '" >' + settings.text + '</span>');
 
-        $(containerIDhash).hide().click(scrollToTop);
-
-        var scrollToTop = function () {
+        $(containerIDhash).hide().click(function () {
             $('html, body').stop().animate({scrollTop: 0}, settings.scrollSpeed, settings.easingType);
             $('#' + settings.containerHoverID, this).stop().animate({'opacity': 0}, settings.inDelay, settings.easingType);
-            return false;
-        };
+        });
 
         $(window).scroll(function () {
             var sd = $(window).scrollTop();
